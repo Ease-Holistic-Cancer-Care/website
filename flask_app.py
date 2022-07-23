0256b03ac92d9e2641ff492d15f898769b8882a4
yashvi1713 <yashvishukla1713@gmail.com>
2022-07-23 20:29:42 +0530

    Initialized the template, stylesheet and script files

--- flask_app.py
+++ flask_app.py
@@ -15,5 +15,29 @@ def about():
 def login():
     return render_template('login.html')
 
+@app.route('/doctors')
+def doctors():
+    return render_template('doctors.html')
+
+@app.route('/specialty')
+def specialty():
+    return render_template('specialty.html')
+
+@app.route('/resources')
+def resources():
+    return render_template('resources.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/appointment')
+def appointment():
+    return render_template('appointment.html')
+
+@app.route('/patient')
+def patient(): 
+    return render_template('patient.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
